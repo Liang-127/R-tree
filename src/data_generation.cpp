83dc0dfad4c86abce103edbca172dfb7aadcff3b
Liang-127 <2022080903026@std.uestc.edu.cn>
2024-05-15 00:23:10 +0800

    实现插入和搜索，分割还需修改

---
+++ src/data_generation.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+//
+const int data_num = 10;
+const int data_dimension = 3;
+const int count = 5;
+const int max_range = 47;
+const int max_increment = 97;
+int main()
+{
+    srand((unsigned)time(NULL));
+    for (int n = 0; n < count; n++)
+    {
+        // 先随机该轮次每个维度的范围和增量
+        int *range = (int *)malloc(sizeof(int) * data_dimension);
+        int *increment = (int *)malloc(sizeof(int) * data_dimension);
+        for (int i = 0; i < data_dimension; i++)
+        {
+            range[i] = rand() % max_range;
+            increment[i] = rand() % max_increment;
+        }
+
+        // 随机生成该轮次的数据
+        for (int i = 0; i < data_num; i++)
+        {
+            int *temp = (int *)malloc(sizeof(int) * data_dimension * 2);
+            for (int k = 0; k < data_dimension; k++) // 两个点表示一个高维空间区域
+            {
+                temp[k] = rand() % range[k] + increment[k];
+                temp[k + data_dimension] = rand() % range[k] + increment[k];
+                 // 判断第二个点的每个维度坐标值都大于第一个点
+                 if (temp[k] > temp[k + data_dimension]) k--;
+            }
+            for (int j = 0; j < data_dimension * 2; j++)
+            {
+                cout << temp[j] << ' ';
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
